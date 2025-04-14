7924914dd47b91751a33264155f9d9b605de3d92
N3F1L1M <cordovaluis0311@gmail.com>
2025-04-14 18:06:22 -0500

    Refactor Navbar component to include user authentication and display user first name

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 
-import React from 'react'
+import { currentUser } from '@clerk/nextjs/server'
 
-const page = () => {
+
+
+const page = async () => {
+
+  const user = await currentUser()
+
   return (
 
       <div>
 
+        holas
+        <div>Hello {user?.firstName}</div>
 
-    <div className='h-50 w-100 bg-amber-500 border-emerald-500 border-8'>
+   {/*  <div className='h-50 w-100 bg-amber-500 border-emerald-500 border-8'>
     </div>
 
     <div className='h-50 w-100 bg-amber-500 border-emerald-500 border-8'>
@@ -29,7 +36,7 @@ const page = () => {
     </div>
 
     <div className='h-50 w-100 bg-amber-500 border-emerald-500 border-8'>
-    </div>
+    </div> */}
 
     </div>
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,35 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { UserButton,useUser    } from "@clerk/nextjs";
 
 const Navbar = () => {
-  return (
-    <nav className="bg-gray-900 text-white px-6 py-4  w-full h-15 top-0 left-0 sticky">
-    <div className="flex justify-between items-center">
-      <h1 className="text-xl font-bold">Zcatalogo Administradores</h1>
-      <ul className="flex space-x-4">
-        <li><a href="#" className="hover:text-gray-400">Inicio</a></li>
-        <li><a href="#" className="hover:text-gray-400">Acerca</a></li>
-        <li><a href="#" className="hover:text-gray-400">Contacto</a></li>
-        <li> <UserButton /> </li>
-      </ul>
-    </div>
-  </nav>
-  )
+
+
+   const { isLoaded, isSignedIn, user } = useUser();
+
+  if (isLoaded && isSignedIn && user) {
+
+    console.log(user);
+
+    return (
+      <nav className="bg-gray-900 text-white px-6 py-4  w-full h-15 top-0 left-0 sticky">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold">Zcatalogo Administradores</h1>
+        <h2 className="text-l font-bold">{user.firstName}</h2>
+        <ul className="flex space-x-4">
+          <li><a href="#" className="hover:text-gray-400">Inicio</a></li>
+          <li><a href="#" className="hover:text-gray-400">Acerca</a></li>
+          <li><a href="#" className="hover:text-gray-400">Contacto</a></li>
+          <li> <UserButton /> </li>
+        </ul>
+      </div>
+    </nav>
+    )
+  }
+
+  
+
+
 }
 
 export default Navbar
